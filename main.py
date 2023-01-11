34221f209238f93a1ce396f63678bee6bf214186
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-12 01:39:51 +0330

    create: Simple-Password-Generator

---
+++ main.py
@@ -0,0 +1,40 @@
+from random import choices
+import string
+import argparse
+
+
+class PasswordGenerator:
+
+    def create_password(length=12, upper=False, lower=False, digit=False, pun=False):
+        character = ''
+
+        if upper:
+            character += string.ascii_uppercase
+        if lower:
+            character += string.ascii_lowercase
+        if digit:
+            character += string.digits
+        if pun:
+            character += string.punctuation
+        if character == '':
+            character += string.ascii_letters
+
+        return ''.join(choices(character, k=length))
+
+
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser(description='Password Generator')
+    parse.add_argument('length', type=int, help='Length of Password')
+    parse.add_argument(
+        '-u', '--upper', help='upper case character', action='store_true')
+    parse.add_argument(
+        '-l', '--lower', help='lower case character', action='store_true')
+    parse.add_argument(
+        '-d', '--digit', help='digit case character', action='store_true')
+    parse.add_argument(
+        '-p', '--pun', help='pun case character', action='store_true')
+    args = parse.parse_args()
+
+    print(PasswordGenerator.create_password(
+        length=args.length, upper=args.upper, lower=args.lower, digit=args.digit, pun=args.pun
+    ))
