5a1968182ef56213680ca869645702eaa8c429b6
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-12 23:24:03 +0330

    function base

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ import string
 import argparse
 
 
-class PasswordGenerator:
+def generator(length=12, upper=False, lower=False, digit=False, pun=False):
+    character = ''
 
-    def create_password(length=12, upper=False, lower=False, digit=False, pun=False):
-        character = ''
+    if upper:
+        character += string.ascii_uppercase
+    if lower:
+        character += string.ascii_lowercase
+    if digit:
+        character += string.digits
+    if pun:
+        character += string.punctuation
+    if character == '':
+        character += string.ascii_letters
 
-        if upper:
-            character += string.ascii_uppercase
-        if lower:
-            character += string.ascii_lowercase
-        if digit:
-            character += string.digits
-        if pun:
-            character += string.punctuation
-        if character == '':
-            character += string.ascii_letters
-
-        return ''.join(choices(character, k=length))
+    return ''.join(choices(character, k=length))
 
 
-if __name__ == '__main__':
+def main():
     parse = argparse.ArgumentParser(description='Password Generator')
     parse.add_argument('length', type=int, help='Length of Password')
     parse.add_argument(
@@ -33,8 +31,17 @@ if __name__ == '__main__':
         '-d', '--digit', help='digit case character', action='store_true')
     parse.add_argument(
         '-p', '--pun', help='pun case character', action='store_true')
-    args = parse.parse_args()
 
-    print(PasswordGenerator.create_password(
-        length=args.length, upper=args.upper, lower=args.lower, digit=args.digit, pun=args.pun
-    ))
+    args = parse.parse_args()
+    password = generator(
+        length=args.length,
+        upper=args.upper,
+        lower=args.lower,
+        digit=args.digit,
+        pun=args.pun)
+
+    print(password)
+
+
+if __name__ == '__main__':
+    main()
